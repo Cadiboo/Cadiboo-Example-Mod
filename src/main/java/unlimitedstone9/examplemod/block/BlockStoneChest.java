084d3f7782e29bbc1dbce84de7200297d7a44958
UnlimatedStone9 <40124890+UnlimatedStone9@users.noreply.github.com>
2018-12-16 04:23:09 +0000

    Create BlockChestblock.java
    Changed block name

---
+++ src/main/java/unlimitedstone9/examplemod/block/BlockStoneChest.java
@@ -0,0 +1,15 @@
+package unlimitedstone9.examplemod.block;
+
+import unlimitedstone9.examplemod.util.ModUtil;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BlockStoneChest extends Block {
+
+	public BlockStoneChest(String name) {
+		super(Material.ROCK);
+		ModUtil.setRegistryNames(this, name);
+		// TODO Auto-generated constructor stub
+	}
+
+}
